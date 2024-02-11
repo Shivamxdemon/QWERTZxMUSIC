ad97ee44788f5d37162c02e6ce98484d4fb0dece
Shivamxdemon <158847420+Shivamxdemon@users.noreply.github.com>
2024-02-11 21:51:40 +0530

    Update __main__.py

--- SankiMusic/__main__.py
+++ SankiMusic/__main__.py
@@ -69,5 +69,5 @@ async def init():
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
+    asyncio.get_event_loop().run_until_complete(init())
     LOGGER("SankiMusic").info("Stopping Music Bot...")
