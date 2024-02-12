ccae081ae6cf7cf999893bb7f81ec9e9dcb5f3d2
Shivamxdemon <158847420+Shivamxdemon@users.noreply.github.com>
2024-02-12 12:51:16 +0530

    Update call.py

--- SankiMusic/modules/core/call.py
+++ SankiMusic/modules/core/call.py
@@ -11,7 +11,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (AlreadyJoinedError,
                                   NoActiveGroupCall) 
-from ntgcalls TelegramServerError
+from ntgcalls import TelegramServerError
 from pytgcalls.types import (JoinedGroupCallParticipant,
                              LeftGroupCallParticipant, Update)
 from pytgcalls.types.input_stream import AudioImagePiped, AudioPiped, AudioVideoPiped
