5fe243ecdca346032a69619940ed5fad7de31cbb
Shivamxdemon <158847420+Shivamxdemon@users.noreply.github.com>
2024-02-12 16:24:27 +0530

    Update sdb.py

--- SankiMusic/modules/core/sdb.py
+++ SankiMusic/modules/core/sdb.py
@@ -1,12 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from SankiMusic.utilities.config import MONGO_DB_URI
+from SankiMusic.utilities.config import MONGO_DB_URL
 
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URL)
     mongodb = _mongo_async_.Anon
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
