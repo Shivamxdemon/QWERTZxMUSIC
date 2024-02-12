b09b35dd1b70e87653e1353dda8dcef647ebb696
Shivamxdemon <158847420+Shivamxdemon@users.noreply.github.com>
2024-02-12 16:08:13 +0530

    Update sdb.py

--- SankiMusic/modules/core/sdb.py
+++ SankiMusic/modules/core/sdb.py
@@ -1,14 +1,14 @@
-# Sanki Database
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from SankiMusic.utilities import config
-from ...console import LOGGER
+from SankiMusic.utilities.config import MONGO_DB_URI
 
-from pymongo import MongoClient
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from ..logging import LOGGER
 
-
-
-_mongo_async_ = _mongo_client_(config.MONGO_DB_URL)
-_mongo_sync_ = MongoClient(config.MONGO_DB_URL)
-mongodb = _mongo_async_.Rose
-pymongodb = _mongo_sync_.Rose
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
