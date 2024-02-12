3683200973ca1e9423bc5e4fe551d606c755aa1b
Shivamxdemon <158847420+Shivamxdemon@users.noreply.github.com>
2024-02-12 23:06:00 +0530

    Update auth.py

--- SankiMusic/plugins/admins/auth.py
+++ SankiMusic/plugins/admins/auth.py
@@ -25,61 +25,28 @@ AUTHUSERS_COMMAND = get_command("AUTHUSERS_COMMAND")
 async def auth(client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text("**» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜsᴇʀ's ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ ᴜsᴇʀɴᴀᴍᴇ/ᴜsᴇʀ_ɪᴅ.**")
-        user = message.text.split(None, 1)[1]
-        if "@" in user:
-            user = user.replace("@", "")
-        user = await bot.get_users(user)
-        user_id = message.from_user.id
-        token = await int_to_alpha(user.id)
-        from_user_name = message.from_user.first_name
-        from_user_id = message.from_user.id
-        _check = await get_authuser_names(message.chat.id)
-        count = len(_check)
-        if int(count) == 20:
-            return await message.reply_text("**» ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ʜᴀᴠᴇ 20 ᴜsᴇʀs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ's ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ (ᴀᴜʟ).**")
-        if token not in _check:
-            assis = {
-                "auth_user_id": user.id,
-                "auth_name": user.first_name,
-                "admin_id": from_user_id,
-                "admin_name": from_user_name,
-            }
-            get = adminlist.get(message.chat.id)
-            if get:
-                if user.id not in get:
-                    get.append(user.id)
-            await save_authuser(message.chat.id, token, assis)
-            return await message.reply_text("**» ᴀᴅᴅᴇᴅ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ʏᴏᴜʀ ɢʀᴏᴜᴘ.**")
-        else:
-            await message.reply_text("**» ᴀʟʀᴇᴀᴅʏ ɪɴ ᴛʜᴇ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ.**")
-        return
-    from_user_id = message.from_user.id
-    user_id = message.reply_to_message.from_user.id
-    user_name = message.reply_to_message.from_user.first_name
-    token = await int_to_alpha(user_id)
-    from_user_name = message.from_user.first_name
+            return await message.reply_text(_["general_1"])
+    user = await extract_user(message)
+    token = await int_to_alpha(user.id)
     _check = await get_authuser_names(message.chat.id)
-    count = 0
-    for smex in _check:
-        count += 1
-    if int(count) == 20:
-        return await message.reply_text("**» ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ʜᴀᴠᴇ 20 ᴜsᴇʀs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ's ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ (ᴀᴜʟ).**")
+    count = len(_check)
+    if int(count) == 25:
+        return await message.reply_text(_["auth_1"])
     if token not in _check:
         assis = {
-            "auth_user_id": user_id,
-            "auth_name": user_name,
-            "admin_id": from_user_id,
-            "admin_name": from_user_name,
+            "auth_user_id": user.id,
+            "auth_name": user.first_name,
+            "admin_id": message.from_user.id,
+            "admin_name": message.from_user.first_name,
         }
         get = adminlist.get(message.chat.id)
         if get:
-            if user_id not in get:
-                get.append(user_id)
+            if user.id not in get:
+                get.append(user.id)
         await save_authuser(message.chat.id, token, assis)
-        return await message.reply_text("**» ᴀᴅᴅᴇᴅ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ʏᴏᴜʀ ɢʀᴏᴜᴘ.**")
+        return await message.reply_text(_["auth_2"].format(user.mention))
     else:
-        await message.reply_text("**» ᴀʟʀᴇᴀᴅʏ ɪɴ ᴛʜᴇ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ.**")
+        return await message.reply_text(_["auth_3"].format(user.mention)) 
 
 
 @bot.on_message(
